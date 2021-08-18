31522b233ed97d624d84a0a0d775d31f5a387309
Alexis Matheron <matheron.alexis30@gmail.com>
2021-08-18 22:14:56 +0200

    user delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -45,3 +45,15 @@ module.exports.updateUser = async (req, res) => {
 		return res.status(500).json({ message: err });
 	}
 };
+
+module.exports.deleteUser = async (req, res) => {
+	if (!ObjectID.isValid(req.params.id)) {
+		return res.status(400).send('ID unknown :' + req.params.id);
+	}
+	try {
+		await UserModel.remove({ _id: req.params.id }).exec();
+		res.status(200).json({ message: "Successfully deleted" });
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+}
